940fb79c1308d1ed7756cf9032cdf9f7f33c4ca2
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-02 22:09:05 +0330

    Implementing calcDisplaySummary function

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const displayMovements = function (movements) {
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov}💶</div>
       </div>`;
     containerMovements.insertAdjacentHTML("afterbegin",html)
   })
@@ -75,9 +75,26 @@ displayMovements(account1.movements)
 
 ////////////////////////////////////////////////////////////////////////////////////
 
+const calcDisplaySummary = function (movements) {
+  const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}💶`;
+
+  const outcomes = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov , 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}💶`
+
+  const interest = movements.filter(mov => mov > 0).map(deposit  => deposit * 1.2 / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}💶`
+}
+
+calcDisplaySummary(account1.movements);
+
+////////////////////////////////////////////////////////////////////////////////////
+
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance} 💶`;
 }
 
 calcDisplayBalance(account1.movements);

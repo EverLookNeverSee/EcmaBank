3e76c31bfd8fde59e58503e2f8ea4ef68020e7a7
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-21 18:08:01 +0330

    Setting timeout for loan request

--- script.js
+++ script.js
@@ -212,10 +212,12 @@ btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
   const amount = Math.floor(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    currentAccount.movements.push(amount);
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
-    updateUI(currentAccount);
+    setTimeout(() => {
+      currentAccount.movements.push(amount);
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = "";
 });
